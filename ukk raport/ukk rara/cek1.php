8418abdb43fe0b64bf7ae35807ae83c28d62eb5f
Ilyssr <103397786+sryna19@users.noreply.github.com>
2024-06-01 23:49:29 +0700

    Delete ukk raport/ukk rara directory

--- ukk raport/ukk rara/cek1.php
+++
@@ -1,5 +0,0 @@
-<?php
-if ($_SESSION['log'] == true) {
-} else {
-    echo "<script>alert('Gagal Login'); window.location.href='index.php'</script>";
-}
--- ukk raport/ukk rara/guru/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['nip']);
-session_destroy();
-header('location:../index.php');
-?>
